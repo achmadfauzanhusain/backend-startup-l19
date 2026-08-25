60ec2c8ff9d6e11116a81b7a7d35f98ce2cdf54c
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-08-25 21:09:33 +0700

    delete test

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-const testRouter = require('./app/test/router');
 const authRouter = require("./app/auth/router")
 
 const app = express();
