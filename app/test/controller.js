dfc32402883f4787edc76418a735c356564e7ffe
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-08-25 21:08:47 +0700

    delete test

--- app/test/controller.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    test: async(req, res) => {
-        try {
-            res.status(200).json({ message: "Test successful" });
-        } catch (error) {
-            res.status(500).json({ message: "Internal Server Error" });
-        }
-    }
-}
