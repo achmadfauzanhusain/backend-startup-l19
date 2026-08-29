0bfe35758f5538b8972573c48ec85ae34c775c3b
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-08-29 18:44:33 +0700

    use middleware

--- app/post/router.js
+++ app/post/router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { isLoginUser } = require("../middleware/auth")
 const { createPost } = require("./controller")
 
-router.post("/create", createPost)
+router.post("/create", isLoginUser, createPost)
 
 module.exports = router
