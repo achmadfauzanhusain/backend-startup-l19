563f1f63e50bab1d9093bb45407a626e9bd596aa
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-09-03 14:54:58 +0700

    new controller

---
+++ app/user/controller.js
@@ -0,0 +1,9 @@
+module.exports = {
+    editProfile: async(req, res) => {
+        try {
+
+        } catch(error) {
+            res.stattus(500).json({ message: 'Internal Server Error' });
+        }
+    }
+}
