e598592e6ea5e1a754763fc61950a1c058e6fb3b
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-09-03 14:59:51 +0700

    feat(editProfile)

--- app/user/controller.js
+++ app/user/controller.js
@@ -1,9 +1,21 @@
+const { colUser } = require("../../db/firebase.js")
+
 module.exports = {
     editProfile: async(req, res) => {
         try {
+            const { displayName, bio, link1, link2, link3 } = req.body
 
+            const userRef = doc(colUser, req.user.id)
+            await updateDoc(userRef, { 
+                displayName, 
+                bio, 
+                link1, 
+                link2, 
+                link3 
+            })
+            res.status(200).json({ message: 'Profile updated successfully' })
         } catch(error) {
-            res.stattus(500).json({ message: 'Internal Server Error' });
+            res.status(500).json({ message: 'Internal Server Error' });
         }
     }
 }
--- app/user/router.js
+++ app/user/router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { isLoginUser } = require('../../middleware/auth');
+const { editProfile } = require('./controller');
 
-
+router.put('/profile', isLoginUser, editProfile);
 
 module.exports = router
