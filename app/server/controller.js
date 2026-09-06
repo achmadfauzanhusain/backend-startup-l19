a790390a74222f1cc42415ae2aad7b8a53bf7a40
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-09-06 15:44:15 +0700

    feat(getDetailServer)

--- app/server/controller.js
+++ app/server/controller.js
@@ -1,5 +1,5 @@
 const { runTransaction, where, addDoc, getDoc, getDocs, doc, increment, arrayUnion, arrayRemove, serverTimestamp, query, orderBy } = require("firebase/firestore");
-const { colServer } = require("../../db/firebase.js")
+const { colServer, colPost } = require("../../db/firebase.js")
 
 module.exports = {
     createServer: async(req, res) => {
@@ -18,6 +18,33 @@ module.exports = {
                 createdAt: serverTimestamp()
             })
             res.status(201).json({ message: "successfully created!", data: docRef.id})
+        } catch (error) {
+            res.status(500).json({ message: "Internal Server Error" })
+        }
+    },
+    getAllServers: async(req, res) => {
+        try {
+            const querySnapshot = await getDocs(colServer)
+            const servers = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+            res.status(200).json({ data: servers })
+        } catch (error) {
+            res.status(500).json({ message: "Internal Server Error" })
+        }
+    },
+    getDetailServer: async(req, res) => {
+        try {
+            const { idServer } = req.params
+
+            const docRef = doc(colPost, idServer);
+            const docSnap = await getDoc(docRef);
+
+            if (!docSnap.exists()) {
+                return res.status(404).json({ message: "Data not found" });
+            }
+
+            const personalData = { id: docSnap.id, ...docSnap.data() };
+
+            res.status(200).json({ data: personalData });
         } catch (error) {
             res.status(500).json({ message: "Internal Server Error" })
         }
--- app/server/router.js
+++ app/server/router.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createServer } = require("./controller")
+const { createServer, getAllServers, getDetailServer } = require("./controller")
 const { isLoginUser } = require('../middleware/auth');
 
+router.get("/all", getAllServers)
+router.get("/:idServer", getDetailServer)
 router.post('/create', isLoginUser, createServer)
 
 module.exports = router
