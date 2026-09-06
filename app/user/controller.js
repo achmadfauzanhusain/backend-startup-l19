b4c7c40d4a2abe997e82774c2443bd22077c68e8
achmadfauzanhusain <achmadfauzan1340@gmail.com>
2026-09-06 08:17:28 +0700

    feat(dataUser)

--- app/user/controller.js
+++ app/user/controller.js
@@ -1,13 +1,30 @@
-const { doc, updateDoc } = require("firebase/firestore");
+const { getDoc, doc } = require("firebase/firestore");
 const { colUser } = require("../../db/firebase.js")
 
 module.exports = {
+    dataUser: async(req, res) => {
+        try {
+            const { hashAddress } = req.params;
+
+            const docRef = doc(colUser, hashAddress); // hashAddress = document ID
+            const docSnap = await getDoc(docRef);
+
+            if (!docSnap.exists()) {
+                return res.status(404).json({ message: "Data not found" });
+            }
+
+            const personalData = { id: docSnap.id, ...docSnap.data() };
+
+            res.status(200).json({ data: personalData });
+        } catch (error) {
+            res.status(500).json({ message: "Internal Server Error" })
+        }
+    },
     editProfile: async(req, res) => {
         try {
             const { displayName, bio, link1, link2, link3 } = req.body
 
             const userRef = doc(colUser, req.user.id)
-            console.log(userRef)
             await updateDoc(userRef, { 
                 displayName: displayName ? displayName : "",
                 bio: bio ? bio : "",
--- app/user/router.js
+++ app/user/router.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { isLoginUser } = require('../middleware/auth');
-const { editProfile } = require('./controller');
+const { dataUser, editProfile } = require('./controller');
 
+router.get('/:hashAddress', dataUser)
 router.put('/profile', isLoginUser, editProfile);
 
 module.exports = router
